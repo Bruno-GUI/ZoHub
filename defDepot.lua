1f257c55364e076170ff309162ade3e07766450c
Bruno-GUI <brunoguilhermefs81003@gmail.com>
2023-02-17 17:36:09 -0300

    updato

--- defDepot.lua
+++ defDepot.lua
@@ -8,6 +8,42 @@ local Window = Library.CreateLib("DefDepot HUB", "Ocean")
             loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
         end)
 
+        MainSection:NewButton("Esp Crates", "Put a red dot in crates. JUST CLICK THIS BUTTON ONE TIME!", function()
+            game.Workspace.ChildAdded:connect(function(Thing)
+                if Thing.Name == 'Crate' then
+                local x = Instance.new('BillboardGui',Thing)
+                x.AlwaysOnTop = true
+                x.Size = UDim2.new(1,0,1,0)
+                local b = Instance.new('Frame',x)
+                b.Size = UDim2.new(1,0,1,0)
+                x.Adornee = Thing
+                b.BackgroundColor3 = Color3.new(1,0,0)
+                end
+            end) 
+        end)
+
+        MainSection:NewToggle("AutoCrate", "You'll probably get banned if someone caught you. :)", function(state)
+            if state then
+                _G.Active = true
+
+                while (_G.Active)
+                do task.wait(2)
+                local player = game:GetService("Players").LocalPlayer
+                local character = player.Character
+                character:WaitForChild("HumanoidRootPart").CFrame = game.workspace.Crate.CFrame
+                end        
+            else
+                _G.Active = false
+
+                while (_G.Active)
+                do task.wait(1)
+                local player = game:GetService("Players").LocalPlayer
+                local character = player.Character
+                character:WaitForChild("HumanoidRootPart").CFrame = game.workspace.Crate.CFrame
+                end
+            end
+        end)
+
         MainSection:NewToggle("AutoFight", "This enable auto fight or disable when off.", function(state)
             if state then
                 _G.Active = true
