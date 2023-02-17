9eff011668aa10471114bf25fa7e960989424994
Bruno-GUI <brunoguilhermefs81003@gmail.com>
2023-02-17 02:35:35 -0300

    local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))() local Window = Library.CreateLib("DefDepot HUB", "Ocean")
        local Main = Window:NewTab("Main")
        local MainSection = Main:NewSection("Main")
    
            MainSection:NewButton("Infinite Yield", "FE CMDS", function()
                loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
            end)
    
            MainSection:NewToggle("AutoFight", "This enable auto fight or disable when off.", function(state)
                if state then
                    _G.Active = true
    
                    while (_G.Active)
                    do task.wait(1)
                    local ohString1 = "spawn"
                    local ohBoolean2 = false
                    game:GetService("ReplicatedStorage").Events.StartStage:FireServer(ohString1, ohBoolean2)
                    end
                else
                    _G.Active = false
    
                    while (_G.Active)
                    do task.wait(1)
                    local ohString1 = "spawn"
                    local ohBoolean2 = false
                    game:GetService("ReplicatedStorage").Events.StartStage:FireServer(ohString1, ohBoolean2)
                    end
                end
            end)
    
        local Misc = Window:NewTab("Misc")
        local MiscSection = Misc:NewSection("Misc")
    
        MiscSection:NewKeybind("Key to Hide UI", "", Enum.KeyCode.RightShift, function()
            Library:ToggleUI()
        end)

--- defDepot.lua
+++ defDepot.lua
@@ -1,32 +1,39 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
 local Window = Library.CreateLib("DefDepot HUB", "Ocean")
 
-    local Tab = Window:NewTab("Main")
-    local MainSection = Tab:NewSection("Main")
+    local Main = Window:NewTab("Main")
+    local MainSection = Main:NewSection("Main")
    
-   
-    MainSection:NewButton("Infinite Yield", "FE CMDS", function()
-        loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
-    end)
+        MainSection:NewButton("Infinite Yield", "FE CMDS", function()
+            loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
+        end)
 
-    MainSection:NewToggle("AutoFight", "This enable auto fight or disable when off.", function(state)
-        if state then
-            _G.Active = true
+        MainSection:NewToggle("AutoFight", "This enable auto fight or disable when off.", function(state)
+            if state then
+                _G.Active = true
 
-            while (_G.Active)
-            do task.wait(1)
-            local ohString1 = "spawn"
-            local ohBoolean2 = false
-            game:GetService("ReplicatedStorage").Events.StartStage:FireServer(ohString1, ohBoolean2)
+                while (_G.Active)
+                do task.wait(1)
+                local ohString1 = "spawn"
+                local ohBoolean2 = false
+                game:GetService("ReplicatedStorage").Events.StartStage:FireServer(ohString1, ohBoolean2)
+                end
+            else
+                _G.Active = false
+
+                while (_G.Active)
+                do task.wait(1)
+                local ohString1 = "spawn"
+                local ohBoolean2 = false
+                game:GetService("ReplicatedStorage").Events.StartStage:FireServer(ohString1, ohBoolean2)
+                end
             end
-        else
-            _G.Active = false
+        end)
 
-            while (_G.Active)
-            do task.wait(1)
-            local ohString1 = "spawn"
-            local ohBoolean2 = false
-            game:GetService("ReplicatedStorage").Events.StartStage:FireServer(ohString1, ohBoolean2)
-            end
-        end
+    local Misc = Window:NewTab("Misc")     
+    local MiscSection = Misc:NewSection("Misc")
+        
+    MiscSection:NewKeybind("Key to Hide UI", "", Enum.KeyCode.RightShift, function()
+        Library:ToggleUI()
     end)
+    
