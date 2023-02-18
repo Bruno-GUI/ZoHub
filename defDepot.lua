19262ed7231de7dad413323c29dfd22314fb646a
Bruno-GUI <brunoguilhermefs81003@gmail.com>
2023-02-17 22:13:43 -0300

    updato

--- defDepot.lua
+++ defDepot.lua
@@ -8,7 +8,7 @@ local Window = Library.CreateLib("DefDepot HUB", "Ocean")
             loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
         end)
 
-        MainSection:NewButton("Esp Crates", "Put a red dot in crates. JUST CLICK THIS BUTTON ONE TIME!", function()
+        MainSection:NewButton("Esp Crates", "Put a red dot in crates.", function()
             game.Workspace.ChildAdded:connect(function(Thing)
                 if Thing.Name == 'Crate' then
                 local x = Instance.new('BillboardGui',Thing)
@@ -22,27 +22,35 @@ local Window = Library.CreateLib("DefDepot HUB", "Ocean")
             end) 
         end)
 
-        MainSection:NewToggle("AutoCrate", "You'll probably get banned if someone caught you. :)", function(state)
-            if state then
-                _G.Active = true
-
-                while (_G.Active)
-                do task.wait(2)
-                local player = game:GetService("Players").LocalPlayer
-                local character = player.Character
-                character:WaitForChild("HumanoidRootPart").CFrame = game.workspace.Crate.CFrame
-                end        
-            else
-                _G.Active = false
-
-                while (_G.Active)
-                do task.wait(1)
-                local player = game:GetService("Players").LocalPlayer
-                local character = player.Character
-                character:WaitForChild("HumanoidRootPart").CFrame = game.workspace.Crate.CFrame
-                end
+        MainSection:NewButton("AutoCrate", "Bring all crates to you.", function()
+            game:GetService("Players").LocalPlayer:WaitForChild("Info"):WaitForChild("Gamepasses"):WaitForChild("Auto").Value = true
+            local player = game.Players.LocalPlayer
+            for i,p in pairs(game.Workspace:GetChildren()) do
+              if p.Name == "Crate" then
+                  p.CanCollide = false
+                  p.Position = player.Character.HumanoidRootPart.Position
+              end
             end
+            
+            workspace.ChildAdded:Connect(function(child)
+            if child.Name == "Crate" then
+                  child.CanCollide = false
+                  child.Position = player.Character.HumanoidRootPart.Position
+              end
+            end)
+            
+            spawn(function()
+            while wait(15) do
+            for i,p in pairs(game.Workspace:GetChildren()) do
+              if p.Name == "Crate" then
+                  p.CanCollide = false
+                  p.Position = player.Character.HumanoidRootPart.Position
+            end
+            end
+            end
+            end)   
         end)
+
 
         MainSection:NewToggle("AutoFight", "This enable auto fight or disable when off.", function(state)
             if state then
@@ -66,27 +74,27 @@ local Window = Library.CreateLib("DefDepot HUB", "Ocean")
             end
         end)
 
-    local Miscs = Window:NewTab("Misc")     
-    local MiscsSection = Miscs:NewSection("Misc")
+        local Miscs = Window:NewTab("Misc")     
+        local MiscsSection = Miscs:NewSection("Misc")
 
-    local function codeInput(code)
-        game:GetService("ReplicatedStorage").Events.CheckCode:InvokeServer(code)
-    end
-    
-    --muda teu misc button
-    local miscCodes = {"*%@?", "bingchillin", "65M!", "300kFAVE", "festiveTime", "snowsnosns", "60M!", "DD2", "winteriscoming", "candy", "corn", "soonTM", "tags?!", "redYELLOWred", "spamspam", "settingsnoway", "ty4100k", "Ascensions!", "gaming", "ascend?", "killALLpls", "zoooom", "worldWIDE", "SUPERCHAT", "tahc labolg", "thatswild", "token?", "wal", "FONUFO", "O_o", "ZaP", "speeeed", "daily", "spooky", "bugcatcher", "69"} -- bota o resto dos codes
-    MiscsSection:NewButton("Redeem All Codes", "....", function()
-        for _, code in ipairs(miscCodes) do
-            codeInput(code)
+        local function codeInput(code)
+            game:GetService("ReplicatedStorage").Events.CheckCode:InvokeServer(code)
         end
-    end)
+        
+        --muda teu misc button
+        local miscCodes = {"*%@?", "bingchillin", "65M!", "300kFAVE", "festiveTime", "snowsnosns", "60M!", "DD2", "winteriscoming", "candy", "corn", "soonTM", "tags?!", "redYELLOWred", "spamspam", "settingsnoway", "ty4100k", "Ascensions!", "gaming", "ascend?", "killALLpls", "zoooom", "worldWIDE", "SUPERCHAT", "tahc labolg", "thatswild", "token?", "wal", "FONUFO", "O_o", "ZaP", "speeeed", "daily", "spooky", "bugcatcher", "69"} -- bota o resto dos codes
+        MiscsSection:NewButton("Redeem All Codes", "....", function()
+            for _, code in ipairs(miscCodes) do
+                codeInput(code)
+            end
+        end)
 
-    local Others = Window:NewTab("Others")
-    local OthersSection = Others:NewSection("Others")
+        local Others = Window:NewTab("Others")
+        local OthersSection = Others:NewSection("Others")
 
-    OthersSection:NewKeybind("Toggle UI off", "...", Enum.KeyCode.LeftShift, function()
-        Library:ToggleUI()
-    end)
+        OthersSection:NewKeybind("Toggle UI off", "...", Enum.KeyCode.LeftShift, function()
+            Library:ToggleUI()
+        end)
 
 
 
